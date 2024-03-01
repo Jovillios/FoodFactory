03ddb67b4848e689946919110c432fd331b83e74
g21dumon <gabriel.dumont@imt-atlantique.net>
2024-03-01 21:09:08 +0100

    j'ai faim

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "SushiFactory.h"
 #include "iFood.h"
 #include <iostream>
+#include <vector>
 
 int main() {
     std::cout << "Launching the main program" << std::endl;
@@ -22,7 +23,23 @@ int main() {
     pizzeria.deliver(std::move(pizza));
     domac.deliver(std::move(bigmac));
     sushishop.deliver(std::move(californien));
-    // Cleaning
+    // Now with pointers, vectors and loops
+    std::cout << "Creating more factories" << std::endl;
+    std::vector<std::unique_ptr<FoodFactory>> factories;
+    for (int i = 0; i < 3; i++) {
+        factories.push_back(std::unique_ptr<FoodFactory>(new PizzaFactory()));
+        factories.push_back(std::unique_ptr<FoodFactory>(new SushiFactory()));
+        factories.push_back(std::unique_ptr<FoodFactory>(new BurgerFactory()));
+    }
+    std::cout << "MOOOORE FOOOD" << std::endl;
+    for (auto &factory : factories) {
+        for (int i = 0; i < 5; i++)
+        {
+            factory->deliver(factory->create());
+        }
+        
+    }
+    // Cleanup
     std::cout << "End of main program- destroying heap objects" << std::endl;
     std::cout << "End of main program- destroying stack objects" << std::endl;
     return 0;
