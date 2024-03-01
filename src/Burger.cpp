c0531e7ad6efe8488d8988c84db3200fa870c89c
g21dumon <gabriel.dumont@imt-atlantique.net>
2024-03-01 15:02:54 +0100

    burger

---
+++ src/Burger.cpp
@@ -0,0 +1,10 @@
+#include "Burger.h"
+#include <iostream>
+
+void Burger::prepare() {
+    std::cout << "Preparing Burger" << std::endl;
+}
+
+void Burger::pack() {
+    std::cout << "Packing Burger" << std::endl;
+}
